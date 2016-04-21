4fb749aa4faee8e0db01ea127987c20f159fbfcd
applecool <kmitharsha@gmail.com>
2016-04-21 11:27:47 -0500

    fixed nlp

--- PublishMagz/html/recommendations.php
+++ PublishMagz/html/recommendations.php
@@ -1,8 +1,5 @@
 <?php
 
-// This file is the home page.
-// This script is begun in Chapter 3.
-
 // Require the configuration before any PHP code as the configuration controls error reporting:
 require('./includes/config.inc.php');
 // The config file also starts the session.
@@ -16,7 +13,6 @@ $_SESSION['user_not_expired'] = true;
 // Require the database connection:
 require(MYSQL);
 
-// Next block added in Chapter 4:
 // If it's a POST request, handle the login attempt:
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	include('./includes/login.inc.php');
@@ -65,17 +61,26 @@ while (list($id, $category) = mysqli_fetch_array($r, MYSQLI_NUM)) {
  	} else{
  		 echo'<p class="lead">Welcome to your recommendations page, here are your personalized recommendations based on your published posts.</p>';
  		
- 		$category_list= rtrim($category_list,','); 
- 		$q = 'SELECT DISTINCT category FROM `categories` WHERE id in (
-SELECT CG.child FROM `category_grouping` CG INNER JOIN `categories` C ON C.id = CG.parent WHERE C.category IN ('.$category_list.')) AND category NOT IN ('.$category_list.')';
-$r = mysqli_query($dbc, $q);
-echo '<ul>';
-while (list( $category) = mysqli_fetch_array($r, MYSQLI_NUM)) {
-	echo '<li>'.$category. '</li>';
+//  		$category_list= rtrim($category_list,','); 
+//  		$q = 'SELECT DISTINCT category FROM `categories` WHERE id in (
+// SELECT CG.child FROM `category_grouping` CG INNER JOIN `categories` C ON C.id = CG.parent WHERE C.category IN ('.$category_list.')) AND category NOT IN ('.$category_list.')';
+// $r = mysqli_query($dbc, $q);
+// echo '<ul>';
+// while (list( $category) = mysqli_fetch_array($r, MYSQLI_NUM)) {
+// 	echo '<li>'.$category. '</li>';
 	
-}
-echo '</ul>';
-
+// }
+// echo '</ul>';
+ 		 $final_content = '';
+ 		 $q = "SELECT content FROM `pages` WHERE `Created_By` = '".$_SESSION['username']."'";
+ 		 $r = mysqli_query($dbc, $q);
+ 		 while(list($c) = mysqli_fetch_array($r, MYSQL_NUM)){
+ 		 	$final_content = $final_content.$c;
+ 		 }
+ 		 echo $final_content;
+ 		 $data = $final_content;
+ 		 $result = shell_exec('python "/Applications/XAMPP/htdocs/pos/postagger.py" ' . escapeshellarg(json_encode($data)));
+ 		 echo $result;
  	}
  ?>
 
