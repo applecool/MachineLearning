0d5d3a5952215d913aa2cb599b11b7fe898d38ed
applecool <kmitharsha@gmail.com>
2016-04-27 19:22:36 -0500

    fixed the recommendations.php

--- PublishMagz/html/recommendations.php
+++ PublishMagz/html/recommendations.php
@@ -21,20 +21,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 include('./includes/header.html');
 
 ?>
-<!-- Begin page content -->
-      <div class="container">
+    <!-- Begin page content -->
+    <div class="container">
 
-		<div class="row">
+        <div class="row">
 
-			<div class="col-3">
-			<?php // Should we show the login form?
+            <div class="col-3">
+                <?php // Should we show the login form?
 if (!isset($_SESSION['user_id'])) {
 	require('includes/login_form.inc.php');
 }
 ?>
-				<h3 class="text-success"> User Content</h3>
-			<div class="list-group">
-<?php // Dynamically generate the content links:
+                    <h3 class="text-success"> User Content</h3>
+                    <div class="list-group">
+                        <?php // Dynamically generate the content links:
 $q = 'SELECT DISTINCT C.id, C.category FROM `categories` C INNER JOIN `pages` P ON C.id = P.categories_id WHERE p.Created_By ="'.$_SESSION['username'].'" ORDER BY category';
 $r = mysqli_query($dbc, $q);
 $category_list = '';
@@ -44,17 +44,19 @@ while (list($id, $category) = mysqli_fetch_array($r, MYSQLI_NUM)) {
 	$category_list = $category_list."'".$category."',";
 }
 ?>
-			 
-			</div><!--/list-group-->
 
-			</div><!--/col-3-->
+                    </div>
+                    <!--/list-group-->
+
+            </div>
+            <!--/col-3-->
 
 
-		  <div class="col-9">
-			<!-- CONTENT -->
+            <div class="col-9">
+                <!-- CONTENT -->
 
-<h1>Recommendations</h1>
- <?php
+                <h1>Recommendations</h1>
+                <?php
  	if(mysqli_num_rows($r) === 0){
  		echo '<p class="lead">You have 0 Publications. Please create new publications to get personalized recommendations.</p>';
  		
@@ -74,18 +76,23 @@ while (list($id, $category) = mysqli_fetch_array($r, MYSQLI_NUM)) {
  		 $final_content = '';
  		 $q = "SELECT content FROM `pages` WHERE `Created_By` = '".$_SESSION['username']."'";
  		 $r = mysqli_query($dbc, $q);
+ 		 echo '<ul>';
  		 while(list($c) = mysqli_fetch_array($r, MYSQL_NUM)){
- 		 	$final_content = $final_content.$c;
- 		 }
- 		 echo $final_content;
+ 		 	$final_content = $c;
+ 		 
+ 		 //echo $final_content;
  		 $data = $final_content;
  		 $result = shell_exec('python "/Applications/XAMPP/htdocs/pos/postagger.py" ' . escapeshellarg(json_encode($data)));
- 		 echo $result;
+         echo '<li class="lead">';
+         echo $result;
+         echo '</li>';
+     }
+     echo '</ul>';
  	}
  ?>
 
 
-<?php /* PAGE CONTENT ENDS HERE! */
+                    <?php /* PAGE CONTENT ENDS HERE! */
 
 // Include the footer file to complete the template:
 include('./includes/footer.html');
